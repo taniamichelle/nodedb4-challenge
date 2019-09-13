84fedc34abba15ca8ded6892a64b786b3aad1f5e
tania <tanislavens@yahoo.com>
2019-09-13 10:36:29 -0400

    last two gets not working

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -17,12 +17,38 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    Recipes.getShoppingList()
+    const { id: recipe_id } = req.params;
+    Recipes.getShoppingList(recipe_id)
+        .then(shoppingList => {
+            if (shoppingList) {
+                return res.json(shoppingList);
+            } else {
+                return res.status(404).json({ error: 'Error finding ingredient with that id.' });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: 'Failed to get shoppingList.' });
+        });
 });
 
-router.get('/:id/instructions', (req, res) => {
-
-});
-
+// router.get('/:id/instructions', (req, res) => {
+//     const {id: recipe_id} = req.params;
+//     Recipes.getInstructions(recipe_id)
+//         .then(instructions => {
+//             if()
+//         })
+// });
+// Schemes.findSteps(id)
+//     .then(steps => {
+//         if (steps.length) {
+//             res.json(steps);
+//         } else {
+//             res.status(404).json({ message: 'Could not find steps for given scheme' })
+//         }
+//     })
+//     .catch(err => {
+//         res.status(500).json({ message: 'Failed to get steps' });
+//     });
+// });
 module.exports = router;
